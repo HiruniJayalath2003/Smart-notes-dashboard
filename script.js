970e6213c1342b032c9432d785a6f957d5ccdc35
H.N.Jayalath <hirunijayalath74@gmail.com>
2026-04-24 23:15:05 +0530

    create note cards and update javascript

---
+++ script.js
@@ -0,0 +1,58 @@
+
+let notes = [
+    {
+        name: "Project Deadline",
+        desc: "Finish web development project before friday",
+        level: "Important"
+    },
+    {
+        name: "React hooks",
+        desc: "Most used react hooks-useState, udeRef, useEffect",
+        level: "Study"
+    }
+];
+
+const noteForm = document.querySelector(".note-form");
+const addNote = document.querySelector(".note-list");
+
+function renderNotes(noteList) {//create funtion
+
+    const color = noteList.level === "Important" ? "bg-red-500 text-white" :
+        noteList.level === "Study" ? "bg-fuchsia-800 text-white" :
+            noteList.level === "Work" ? "bg-lime-500" :
+                "bg-cyan-500 text-white";
+
+    const taskHTML = `
+    <div class="noteCard border rounded-md p-5 border-gray-400">
+     <h1 class="heading text-[20px] text-blue-800 border-b-1">${noteList.name}</h1>
+     <div class="descBar flex items-center justify-between py-5">
+      <p class="noteDescription text-[17px] text-gray-800">${noteList.desc}</p>
+      <p class="priority border rounded-[15px] px-1 py-0.5 border-gray-500 ${color}">${noteList.level}</p>
+      </div>
+     </div>
+    
+    `;
+    addNote.innerHTML += taskHTML;
+}
+notes.forEach(noteList => renderNotes(noteList));
+noteForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const title = document.querySelector(".title");
+    const addDesc = document.querySelector(".add-desc");
+    const addLevel = document.querySelector(".level");
+
+    let newNotes = {
+        name: title.value,
+        desc: addDesc.value,
+        level: addLevel.value
+    }
+    notes.push(newNotes);
+    renderNotes(newNotes);
+    title.value = '';
+    addDesc.value = '';
+    addLevel.value = '';
+
+});
+
+
