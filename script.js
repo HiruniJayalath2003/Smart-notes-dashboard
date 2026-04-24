0901e9d014582485f4bda0c6522e80d6ecfa2b28
H.N.Jayalath <hirunijayalath74@gmail.com>
2026-04-24 23:20:55 +0530

    fix some color issues

--- script.js
+++ script.js
@@ -17,17 +17,17 @@ const addNote = document.querySelector(".note-list");
 
 function renderNotes(noteList) {//create funtion
 
-    const color = noteList.level === "Important" ? "bg-red-500 text-white" :
-        noteList.level === "Study" ? "bg-fuchsia-800 text-white" :
-            noteList.level === "Work" ? "bg-lime-500" :
-                "bg-cyan-500 text-white";
+    const color = noteList.level === "Important" ? "bg-red-500 text-white border-red-500" :
+        noteList.level === "Study" ? "bg-fuchsia-800 text-white border-fuchisa-800" :
+            noteList.level === "Work" ? "bg-lime-500 border-lime-500" :
+                "bg-cyan-500 text-white-border-blue-500";
 
     const taskHTML = `
     <div class="noteCard border rounded-md p-5 border-gray-400">
      <h1 class="heading text-[20px] text-blue-800 border-b-1">${noteList.name}</h1>
      <div class="descBar flex items-center justify-between py-5">
       <p class="noteDescription text-[17px] text-gray-800">${noteList.desc}</p>
-      <p class="priority border rounded-[15px] px-1 py-0.5 border-gray-500 ${color}">${noteList.level}</p>
+      <p class="priority border rounded-[15px] px-1 py-0.5 ${color}">${noteList.level}</p>
       </div>
      </div>
     
